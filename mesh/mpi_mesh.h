50160b49903e9eb0f7959146e784426088bd7458
KonovDS <konov1999@gmail.com>
2022-03-31 03:21:44 +0300

    Starting development of MPIMesh class

---
+++ mesh/mpi_mesh.h
@@ -0,0 +1,77 @@
+#ifndef IMPLICITSTEFANPROBLEMMPI_MESH_MPI_MESH_H_
+#define IMPLICITSTEFANPROBLEMMPI_MESH_MPI_MESH_H_
+
+#include "mesh.h"
+
+#include <mpi.h>
+
+#include <stdexcept>
+
+class NoMPIException : std::exception {
+  const char * what() const noexcept override {
+    return "MPI was not initialized correctly";
+  }
+};
+
+class MPIRecvError : std::exception {
+  const char * what() const noexcept override {
+    return "MPI Recv failed";
+  }
+};
+
+class MPISendError : std::exception {
+  const char * what() const noexcept override {
+    return "MPI Send failed";
+  }
+};
+
+template<class T>
+class MPIMesh : public Mesh<T> {
+  int world_rank_, world_size_;
+ public:
+  MPIMesh(int individual_k, int l) : Mesh<T>(individual_k, l) {
+    if (MPI_Comm_size(MPI_COMM_WORLD, &world_size_) != MPI_SUCCESS)
+      throw NoMPIException();
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank_);
+  }
+
+  inline void SendLeft() {
+    auto d = this->LeftPtrSend();
+    auto size = this->BorderLen() * sizeof(T);
+    auto tag = world_rank_;
+    auto dest = world_rank_ - 1;
+    if (MPI_Send((void *)d, size, MPI_BYTE, dest, tag, MPI_COMM_WORLD) != MPI_SUCCESS)
+      throw MPISendError();
+  }
+
+  inline void SendRight() {
+    auto d = this->RightPtrSend();
+    auto size = this->BorderLen() * sizeof(T);
+    auto tag = world_rank_;
+    auto dest = world_rank_ + 1;
+    if (MPI_Send((void *)d, size, MPI_BYTE, dest, tag, MPI_COMM_WORLD) != MPI_SUCCESS)
+      throw MPISendError();
+  }
+
+  inline void RecvLeft() {
+    auto d = this->LeftPtrRecv();
+    auto size = this->BorderLen() * sizeof(T);
+    auto tag = world_rank_;
+    auto src = world_rank_ - 1;
+    MPI_Status status;
+    if (MPI_Recv((void *)d, size, MPI_BYTE, src, tag, MPI_COMM_WORLD, &status) != MPI_SUCCESS)
+      throw MPIRecvError();
+  }
+
+  inline void RecvRight() {
+    auto d = this->LeftPtrRecv();
+    auto size = this->BorderLen() * sizeof(T);
+    auto tag = world_rank_;
+    auto src = world_rank_ + 1;
+    MPI_Status status;
+    if (MPI_Recv((void *)d, size, MPI_BYTE, src, tag, MPI_COMM_WORLD, &status) != MPI_SUCCESS)
+      throw MPIRecvError();
+  }
+};
+
+#endif //IMPLICITSTEFANPROBLEMMPI_MESH_MPI_MESH_H_
