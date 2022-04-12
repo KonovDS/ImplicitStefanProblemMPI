a304a5b0bab72d4e39ed03acc93ca7328f69c7ba
KonovDS <konov1999@gmail.com>
2022-04-12 18:39:56 +0300

    Fixed public test not compiling

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -1,4 +1,12 @@
 //
 // Created by user on 7 апр. 2022 г..
 //
+#include "utils.h"
 
+#include <mpi.h>
+
+bool IsMainMPINode() {
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  return world_rank == 0;
+}
--- utils/utils.h
+++ utils/utils.h
@@ -3,10 +3,6 @@
 
 #include <mpi.h>
 
-bool IsMainMPINode() {
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  return world_rank == 0;
-}
+bool IsMainMPINode();
 
 #endif //IMPLICITSTEFANPROBLEMMPI_UTILS_UTILS_H_
