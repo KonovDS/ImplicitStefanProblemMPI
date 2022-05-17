306a2263d3f5c99e4f67d6eeee548c34072f38c5
KonovDS <konov1999@gmail.com>
2022-05-18 02:15:45 +0300

    Added placeholder test_basic_parabolic.cpp

---
+++ tests/test_basic_parabolic.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_FAST_COMPILE
+#include "lib/catch.hpp"
+
+#include <iostream>
+
+#include <mpi.h>
+#include <omp.h>
+
+#include "basic_parabolic.h"
+#include "print.h"
+
+TEST_CASE("Basic_parabolic solver test", "[mpi]") {
+
+}
